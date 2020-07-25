98029ef8be138954d0121c887329bc874e78196d
Travis CI User <travis@example.org>
2020-07-25 11:29:25 +0000

    miui_LAVENDER_20.7.23_1fd155b9c4_10.0.zip

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:67108864:ebe7893c7a1f948a906179c8938f165e73995bbc; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:67108864:e0f038f34ccb239a78709cdb287ad94faf62720b; then
   applypatch  \
           --patch /system/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot:67108864:85ab01e39b5b04a4c843112c90bc1932df4db185 \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery:67108864:ebe7893c7a1f948a906179c8938f165e73995bbc && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot:67108864:7fb2a355373571001b10894f34b6c39069245eb7 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery:67108864:e0f038f34ccb239a78709cdb287ad94faf62720b && \
       log -t recovery "Installing new recovery image: succeeded" && \
         echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
       (log -t recovery "Installing new recovery image: failed" && \
